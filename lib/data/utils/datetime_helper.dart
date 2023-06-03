8cb590667f8b21666fa03abf5e6603f203199cea
grishin <isip_p.s.grishin@mpt.ru>
2023-06-03 18:36:50 +0300

    Исправлен вывод времени

--- lib/data/utils/datetime_helper.dart
+++ lib/data/utils/datetime_helper.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
 class DateTimeHelper {
-  static String getDate(DateTime date) => DateFormat('dd.MM.yyyy').format(date);
-  static String getDateWithTime(DateTime date) => DateFormat('dd.MM.yyyy HH:mm').format(date);
+  static String getDate(DateTime date) => DateFormat('dd.MM.yyyy').format(date.toLocal());
+  static String getDateWithTime(DateTime date) => DateFormat('dd.MM.yyyy HH:mm').format(date.toLocal());
 }
